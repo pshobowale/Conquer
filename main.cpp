cd379a409c78080c7e3ad4c7bbed07cd57aaec3b
Peter <pshobowale@gmail.com>
2022-02-07 18:47:29 +0100

    Added simple example to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
-#include "lib/Engine/Engine.hpp"
+#include <SFML/Graphics.hpp>
 
-int main(int, char**) {
-    std::cout << "Hello, world!"<<WINDOW_TITLE;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
